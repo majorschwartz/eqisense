4fc425905dc89e0b34e4e7ffb12ebb752e222c84
shucksow <102425362+shucksow@users.noreply.github.com>
2024-09-28 09:53:16 -0400

    style: Positionsearch svg inside input field

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,25 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+const SearchIcon = () => (
+	<svg
+		role="img"
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width="24px"
+		height="24px"
+	>
+		<path
+			fill="none"
+			stroke="#000000"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth="2"
+			d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0-14 0m18 11l-6-6"
+		/>
+	</svg>
+);
+
 const Home = () => {
 	const [ticker, setTicker] = useState("");
 	const navigate = useNavigate();
@@ -19,13 +38,18 @@ const Home = () => {
 				<div className="main-text">
 					<span>Enter a stock ticker...</span>
 				</div>
-				<input
-					type="text"
-					value={ticker}
-					onChange={(e) => setTicker(e.target.value)}
-				/>
-				<div className="search-button">
-					<button onClick={() => handleSubmit(ticker)}>Search</button>
+				<div className="search-container">
+					<div className="input-wrapper">
+						<input
+							type="text"
+							value={ticker}
+							onChange={(e) => setTicker(e.target.value)}
+							maxLength="5"
+						/>
+						<button className="search-icon" onClick={() => handleSubmit(ticker)}>
+							<SearchIcon />
+						</button>
+					</div>
 				</div>
 				<div className="popular-tickers">
 					<span>Popular Tickers</span>
