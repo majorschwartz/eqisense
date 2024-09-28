ca65337d07c46164fdadf54aa0275ead8dfdb9b0
Major <107882188+qubitflip@users.noreply.github.com>
2024-09-28 15:58:19 -0400

    Risk desc and title add

--- client/src/components/Risk.js
+++ client/src/components/Risk.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "./Card";
 
-const Risk = ({ analyst_score, in_the_week_score }) => {
+const Risk = ({ analyst_score, in_the_week_score, in_the_week_desc }) => {
 	return (
 		<div className="risk-container">
 			<div className="risk-title">Risk Scores</div>
@@ -12,6 +12,12 @@ const Risk = ({ analyst_score, in_the_week_score }) => {
 				<div className="risk-card second">
 					<Card title="Week" score={in_the_week_score} />
 				</div>
+			</div>
+			<div className="risk-desc">
+				<div className="risk-desc-title">Analyst</div>
+				<div className="risk-desc-text">Top Smart Score Stocks displays the best stocks according to the <a href="https://www.tipranks.com/glossary/s/smart-score">TipRanks Smart Score</a>. This unique score measures stocks on their potential to outperform the market, based on 8 key factors. These include how the best performing analysts are rating stocks, whether hedge funds are buying or selling, as well as fundamental and technical factors.</div>
+				<div className="risk-desc-title">In the Week</div>
+				<div className="risk-desc-text">{in_the_week_desc}</div>
 			</div>
 		</div>
 	);
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,13 +1,16 @@
 import Header from "components/Header";
 import Metrics from "components/Metrics";
 import Risk from "components/Risk";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { getStockData } from "utils/api";
 
 const Main = () => {
     const { ticker } = useParams();
     const navigate = useNavigate();
+    const [analyst_score, setAnalystScore] = useState("4");
+    const [in_the_week_score, setInTheWeekScore] = useState("10");
+    const [inTheWeekDesc, setInTheWeekDesc] = useState("In the Week Description");
 
     // useEffect(() => {
     //     const isValidTicker = (tick) => {
@@ -25,7 +28,7 @@ const Main = () => {
         <div className="main-container">
             <Header />
             <Metrics />
-            <Risk analyst_score={"4"} in_the_week_score={"10"} />
+            <Risk analyst_score={analyst_score} in_the_week_score={in_the_week_score} in_the_week_desc={inTheWeekDesc} />
         </div>
     );
 }
